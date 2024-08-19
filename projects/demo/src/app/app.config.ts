1ad36984f8fb6def1f79b0bff4ddd14dac10ea2b
Thor Christian Koustrup <thor.koustrup@man-es.com>
2024-08-19 13:06:33 +0200

    .
    .

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideLayout } from "../../../lib/src/lib/providers";
+import { provideAnimations } from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideLayout({ titleSuffix: "My App" }),
+    provideAnimations(),
   ],
 };
