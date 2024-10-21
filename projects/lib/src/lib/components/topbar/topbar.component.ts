35da614b9eaf1b83261e59beddc973dc9ffc0cdb
Mikkel Christensen <41455260+Mikkelmmc@users.noreply.github.com>
2024-10-21 13:48:36 +0200

    Fjernet helpService
    Virkede ikke

--- projects/lib/src/lib/components/topbar/topbar.component.ts
+++ projects/lib/src/lib/components/topbar/topbar.component.ts
@@ -1,8 +1,6 @@
 import { Component, Input, ViewChild, ViewContainerRef, ComponentFactoryResolver } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ThemeService } from "../../theme.service";
-import { HelpButtonComponent } from "./helpButton/helpButton.component";
-import { HelpService } from "./helpButton/helpService";
 
 @Component({
   selector: "ngx-layout-topbar",
@@ -13,15 +11,6 @@ import { HelpService } from "./helpButton/helpService";
 export class TopbarComponent {
   @Input({ required: true }) topbarHeight!: number;
 
-  @ViewChild('helpContainer', { read: ViewContainerRef }) helpContainer!: ViewContainerRef;
-
-  constructor(public themeService: ThemeService, private helpService: HelpService, private resolver: ComponentFactoryResolver) {
-    this.helpService.setHelpComponentLoader(this.loadHelpComponent.bind(this));
-  }
-
-  loadHelpComponent() {
-    const factory = this.resolver.resolveComponentFactory(HelpButtonComponent);
-    this.helpContainer.clear(); // Clear previous help content
-    this.helpContainer.createComponent(factory); // Dynamically load HelpButtonComponent
+  constructor(public themeService: ThemeService) {
   }
 }
