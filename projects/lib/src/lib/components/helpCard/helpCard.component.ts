96bb56bbc54b2c01f301e20c749d7c648c866ba8
Mikkel Mühlendorph Christensen <41455260+Mikkelmmc@users.noreply.github.com>
2024-10-30 11:15:33 +0100

    Fixes both helpcard and logo
    0.02
    0.03

--- projects/lib/src/lib/components/helpCard/helpCard.component.ts
+++ projects/lib/src/lib/components/helpCard/helpCard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { ThemeService } from "../../theme.service";
 import { KbdComponent } from "../kbd/kbd.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "help-card",
   templateUrl: "./helpCard.component.html",
   standalone: true,
-  imports: [KbdComponent],
+  imports: [KbdComponent, CommonModule],
 })
 export class HelpComponent {
   constructor(public themeService: ThemeService) {}
